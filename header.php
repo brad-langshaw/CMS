ca961637fef4cbff4e87b788b8e55c1291ea2fbb
brad-langshaw <brad.langshaw@live.ca>
2016-04-05 20:37:24 -0400

    hmmmm

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
 			echo '<li><a href="addpage.php" title="Add">Add Page</a></li>
 				 <li><a href="userlistings.php" title="List">User Listings</a></li>
 				 <li><a href="logout.php" title="Logout">Logout</a></li>';
-             while($row = $pages){
+             while($row =  mysql_fetch_array($pages)){
                     echo '<li><a href="main.php?page_id='.$row['page_id'].'">'.$row['pageName'].'</a></li>'; 
             }
 		}
